6da757b0e9f55afd716c122c53290de5a48b24d4
Beltowski <bradbeltowski@gmail.com>
2018-10-24 16:02:28 -0400

    Set up config keys for the enviroments

---
+++ src/config/index.js
@@ -0,0 +1,17 @@
+// const dev = require('./dev');
+const prod = require('./prod');
+
+const appname = 'Poovey';
+
+const settings = {
+  applicationName: appname
+};
+
+// Figures out what set of credentials to return based on enviroment
+switch (process.env.NODE_ENV) {
+  case 'development':
+    module.exports = dev(settings);
+    break;
+  default:
+    module.exports = prod(settings);
+}
