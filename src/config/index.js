551082d94b24e4dadd76f0d716b35856812c8b5b
Beltowski <bradbeltowski@gmail.com>
2018-10-31 19:20:40 -0400

    Added testing enviroment config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 const dev = require('./dev');
 const prod = require('./prod');
+const test = require('./test');
 
 const appname = 'Poovey';
 
@@ -11,6 +12,9 @@ const settings = {
 switch (process.env.NODE_ENV) {
   case 'development':
     module.exports = dev(settings);
+    break;
+  case 'test':
+    module.exports = test(settings);
     break;
   default:
     module.exports = prod(settings);
---
+++ src/config/test.js
@@ -0,0 +1,6 @@
+module.exports = settings => ({
+  mongodb: {
+    uri: 'mongodb://localhost:27017/how_does_one_unit_test'
+  },
+  ...settings
+});
