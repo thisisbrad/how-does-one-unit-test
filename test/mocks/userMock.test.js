30711de31439a5b49b4a1dd74b60f7d0c747b9ff
Beltowski <bradbeltowski@gmail.com>
2018-10-31 17:32:38 -0400

    Delete and Create done for mocking Mongoose  model

--- test/mocks/userMock.test.js
+++ test/mocks/userMock.test.js
@@ -4,7 +4,15 @@ require('sinon-mongoose');
 
 const User = require('../../src/models/User');
 
+/*  Test Suite TOC
+ #READ   - test suite for fetching with model
+ #UPDATE - test suite for updating with model
+ #DELETE - test suite for deleting the model
+ #CREATE - test suite for creating the model
+ */
+
 describe('Mock Testing of Mongoose Model', () => {
+  // #READ
   describe('Fetch all User data', () => {
     // Test will pass if we get all todos
     it('should return all todos', async () => {
@@ -42,7 +50,7 @@ describe('Mock Testing of Mongoose Model', () => {
     });
   });
 
-  // Test will pass if the todo is updated based on an ID
+  // #UPDATE
   describe("Update a User's todo by id", () => {
     const ricky = new User({
       name: 'Ricky',
@@ -50,6 +58,7 @@ describe('Mock Testing of Mongoose Model', () => {
       todos: [{ _id: 12345, title: 'Setup Hockey Camp', checked: false }]
     });
 
+    // Test will pass if the todo is updated based on an ID
     it('should updated a todo by id', async () => {
       const UserMock = sinon.mock(ricky);
       const todo = UserMock.object;
@@ -81,6 +90,92 @@ describe('Mock Testing of Mongoose Model', () => {
         .yields(expectedResult, null);
 
       await todo.save({ _id: 12345 }, (err, result) => {
+        TodoMock.verify();
+        TodoMock.restore();
+        assert.isNull(result);
+        assert.isNotEmpty(err);
+        assert.property(err, 'error');
+      });
+    });
+  });
+
+  // #DELETE
+  describe('Delete a todo by id', () => {
+    const ricky = new User({
+      name: 'Ricky',
+      email: 'ricky@sunnyvale.com',
+      todos: [{ _id: 12345, title: 'Setup Hockey Camp', checked: false }]
+    });
+
+    // Test will pass if the todo is deleted based on an ID
+    it('should delete a todo by id', async () => {
+      const UserMock = sinon.mock(ricky);
+      const user = UserMock.object;
+      const expectedResult = { success: 'Removed 12345 from todos.' };
+      UserMock.expects('remove')
+        .withArgs({ _id: 12345 })
+        .yields(null, expectedResult);
+      user.remove({ _id: 12345 }, (err, result) => {
+        UserMock.verify();
+        UserMock.restore();
+        assert.property(result, 'success');
+      });
+    });
+
+    // Test will pass if the todo is not deleted based on an ID
+    it('should return error if delete action is failed', async () => {
+      const UserMock = sinon.mock(ricky);
+      const user = UserMock.object;
+      const expectedResult = { error: 'Connection Timed Out' };
+
+      UserMock.expects('remove')
+        .withArgs({ _id: 12345 })
+        .yields(expectedResult, null);
+
+      user.remove({ _id: 12345 }, (err, result) => {
+        UserMock.verify();
+        UserMock.restore();
+        assert.isNull(result);
+        assert.isNotEmpty(err);
+        assert.property(err, 'error');
+      });
+    });
+  });
+
+  // #CREATE
+  describe('Post a new User', () => {
+    const ricky = new User({
+      name: 'Ricky',
+      email: 'ricky@sunnyvale.com',
+      todos: [{ _id: 12345, title: 'Setup Hockey Camp', checked: false }]
+    });
+
+    // Test will pass if the todo is saved
+    it('should create new User', async () => {
+      const UserMock = sinon.mock(ricky);
+      const user = UserMock.object;
+
+      const expectedResult = {
+        name: 'Ricky',
+        email: 'ricky@sunnyvale.com',
+        todos: [{ _id: 12345, title: 'Setup Hockey Camp', checked: false }]
+      };
+      UserMock.expects('save').yields(null, expectedResult);
+      await user.save((err, result) => {
+        UserMock.verify();
+        UserMock.restore();
+        assert.deepEqual(expectedResult, result);
+        assert.hasAllKeys(result, ['name', 'email', 'todos']);
+      });
+    });
+
+    // Test will pass if the todo is not saved
+    it('should return error, if post not saved', async () => {
+      const TodoMock = sinon.mock(ricky);
+      const todo = TodoMock.object;
+      const expectedResult = { error: 'Email is already taken.' };
+      TodoMock.expects('save').yields(expectedResult, null);
+      await todo.save((err, result) => {
         TodoMock.verify();
         TodoMock.restore();
         assert.isNull(result);
