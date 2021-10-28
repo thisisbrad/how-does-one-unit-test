37a75df4d55e6a9e56036986cd4e1c284ce78f87
Brad Beltowski <bradbeltowski@gmail.com>
2021-10-28 19:16:19 -0400

    Add authenication routes to test against.

--- src/config/test.js
+++ src/config/test.js
@@ -1,6 +1,7 @@
 module.exports = settings => ({
   mongodb: {
-    uri: 'mongodb://localhost:27017/how_does_one_unit_test'
+    uri: 'mongodb://localhost:27017/how_does_one_unit_test',
   },
-  ...settings
+  jwt_secret: '3452rfsg5&^@&',
+  ...settings,
 });
