4873bff18a954ae760c0300305ea4f391ad4f7be
Brad Beltowski <bradbeltowski@gmail.com>
2022-10-31 12:58:59 -0400

    Made basic object test with Jest.

--- test/object.test.js
+++ test/object.test.js
@@ -1,20 +1,24 @@
-// describe('User Object', () => {
-//   const user = {}; // declare our user variable so it's in scope for the tests
+describe('User Object', () => {
+  const user = {}; // declare our user variable so it's in scope for the tests
 
-//   before(() => {
-//     // before the tests we make a user object to test with
-//     user.name = 'Ricky';
-//     user.location = 'Sunnyvale';
-//     user.greeting = () => {
-//       console.log('You know the TVs With The Click-Clackers');
-//     };
-//   });
+  beforeEach(() => {
+    // before the tests we make a user object to test with
+    user.name = 'Ricky';
+    user.location = 'Sunnyvale';
+    user.greeting = () => {
+      console.log('You know the TVs With The Click-Clackers');
+    };
+  });
 
-//   describe('properties and methods', () => {
-//     it('should have a name property on the object', () => {});
+  describe('properties and methods', () => {
+    it('should have a name property on the object', () => {
+      expect(user).toHaveProperty('name');
+      expect(user).toHaveProperty('location', 'Sunnyvale');
+    });
 
-//     it('should have a greeting method on the object', () => {});
-
-//     it('should have all the properties on the object', () => {});
-//   });
-// });
+    it('should have a greeting method on the object', () => {
+      expect(user.greeting).toBeDefined();
+      expect(typeof user.greeting).toBe('function');
+    });
+  });
+});
