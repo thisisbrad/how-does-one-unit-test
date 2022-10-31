f5e73137d737fede38d0ac125e9a664fdefa38c0
Brad Beltowski <bradbeltowski@gmail.com>
2022-10-31 13:23:00 -0400

    Jest for Arrays

--- test/array.test.js
+++ test/array.test.js
@@ -1,13 +1,14 @@
-// describe('Array', () => {
-//   describe('#indexOf()', () => {
-//     it('should return -1 when the value is not present', () => {});
+describe('Array', () => {
+  let arr = [8, 3, 6];
 
-//     it('should return 0 when the array is empty', () => {
-//       const arr = [];
-//     });
+  describe('#indexOf()', () => {
+    it('should return -1 when the value is not present', () => {
+      expect(arr).not.toBeUndefined();
+      expect(arr.indexOf(1)).toBe(-1);
+    });
 
-//     it('should return the correct index in the array', () => {
-//       const arr = [1, 2, 3, 4, 5];
-//     });
-//   });
-// });
+    it('should return the correct index in the array', () => {
+      expect(arr.indexOf(3)).toBe(1);
+    });
+  });
+});
