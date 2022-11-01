7fba8e742a4f4b7436b3ce21fb8771954ebb8479
Brad Beltowski <bradbeltowski@gmail.com>
2022-10-31 20:50:18 -0400

    Added Postgres env vars

--- src/config/test.js
+++ src/config/test.js
@@ -1,7 +1,9 @@
 module.exports = settings => ({
-  mongodb: {
-    uri: 'mongodb://localhost:27017/how_does_one_unit_test',
-  },
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
   jwt_secret: '3452rfsg5&^@&',
   ...settings,
 });
